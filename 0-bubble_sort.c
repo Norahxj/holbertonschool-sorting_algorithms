a534147e87deb1c11a5cd42c9c46c575c90d1aff
Norahxj <Nhaljuhani@gmail.com>
2025-12-16 07:23:36 +0000

    Added the code & prototype of Task 0 + Big O notations of Bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers using Bubble sort
+ * @array: Pointer to the array
+ * @size: Number of elements in the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int temp;
+	int swapped;
+
+	if (!array || size < 2)
+		return;
+
+	do {
+		swapped = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+	} while (swapped);
+}
